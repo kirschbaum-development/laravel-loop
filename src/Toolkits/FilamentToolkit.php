dbc62d8eba0dae6ed98b896312b68d92e2fc1a3f
luisdalmolin <403446+luisdalmolin@users.noreply.github.com>
2025-05-19 09:41:58 +0000

    Fix styling

--- src/Toolkits/FilamentToolkit.php
+++ src/Toolkits/FilamentToolkit.php
@@ -2,15 +2,15 @@
 
 namespace Kirschbaum\Loop\Toolkits;
 
-use Kirschbaum\Loop\Enums\Mode;
 use Filament\Resources\Resource;
+use Kirschbaum\Loop\Collections\ToolCollection;
 use Kirschbaum\Loop\Concerns\Makeable;
 use Kirschbaum\Loop\Contracts\Toolkit;
-use Kirschbaum\Loop\Collections\ToolCollection;
-use Kirschbaum\Loop\Tools\Filament\ExecuteResourceActionTool;
-use Kirschbaum\Loop\Tools\Filament\ListFilamentResourcesTool;
-use Kirschbaum\Loop\Tools\Filament\GetFilamentResourceDataTool;
+use Kirschbaum\Loop\Enums\Mode;
 use Kirschbaum\Loop\Tools\Filament\DescribeFilamentResourceTool;
+use Kirschbaum\Loop\Tools\Filament\ExecuteResourceActionTool;
+use Kirschbaum\Loop\Tools\Filament\GetFilamentResourceDataTool;
+use Kirschbaum\Loop\Tools\Filament\ListFilamentResourcesTool;
 
 /**
  * @method static self make(Resource[] $resources, Mode $mode = Mode::ReadOnly)
